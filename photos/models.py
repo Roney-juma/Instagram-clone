af6faf9f6310a0c3a8c1b62178599b34ef0b6a21
Roney-juma <ochirone10@gmail.com>
2021-07-12 08:50:59 +0300

    Created the Post class model

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class Post(models.Model):
+    image = CloudinaryField('image',null=True,blank=True)
+    # image = models.ImageField(upload_to='posts/')
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User , on_delete=models.CASCADE)
