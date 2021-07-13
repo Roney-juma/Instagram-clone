b20690c18c15ca4b9fa0ea7210e14599659d1a11
Roney-juma <ochirone10@gmail.com>
2021-07-13 10:07:52 +0300

    defined the register and search results

--- photos/views.py
+++ photos/views.py
@@ -57,3 +57,20 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin,  DeleteView):
         if self.request.user == post.author:
             return True
         return False
+
+def register(request):
+    return render(request, 'users/register.html')
+
+
+def search_results(request):
+
+    if 'username' in request.GET and request.GET["username"]:
+        search_term = request.GET.get("username")
+        searched_username = Username.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'photo/search.html',{"message":message,"username": searched_username})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'photo/search.html',{"message":message})
