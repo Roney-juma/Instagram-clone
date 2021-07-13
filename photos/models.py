1f10d1e53cb495ad035ba1ae4b5557127a6e84e1
Roney-juma <ochirone10@gmail.com>
2021-07-13 09:07:08 +0300

    created the username class model

--- photos/models.py
+++ photos/models.py
@@ -18,3 +18,30 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('post-detail',kwargs={'pk':self.pk})
+
+
+class Username(models.Model):
+    title = models.CharField(max_length =30)
+    image = CloudinaryField('image',null=True,blank=True)
+    # image = models.ImageField(max_length =60,upload_to='images/')
+    description = models.TextField(max_length =200)
+
+
+
+    def save_username(self):
+        self.save()
+
+    def delete_username(self):
+        self.delete()
+
+    def update_username(self):
+        self.delete()
+
+    @classmethod
+    def search_by_title(cls,search_term):
+        photo = cls.objects.filter(title__icontains=search_term)
+        return photo
+
+
+    def __str__(self):
+        return self.title
