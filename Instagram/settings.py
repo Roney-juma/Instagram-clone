f498cc67e9a8ff7c8cf0126f95c2a94999cdedb1
Roney-juma <ochirone10@gmail.com>
2021-07-13 10:48:24 +0300

    updated the cloudinary configarations

--- Instagram/settings.py
+++ Instagram/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import cloudinary
+from decouple import config,Csv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -39,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'photos',
     'bootstrap3',
+    'users',
+    'cloudinary',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+cloudinary.config( 
+  cloud_name = "roneyjuma", 
+  api_key = "815549592624263", 
+  api_secret = "Frt3q3wztBgp7HDv8Gj4mZtyKFA" 
+)
